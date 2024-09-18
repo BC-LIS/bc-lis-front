d92133ea559b3e38e967ee0b192b2484a8aa782f
Esteban <estebancg12@hotmail.com>
2024-09-18 15:26:19 -0500

    Update zod user schema 🧰

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const formUser = z.object({
   name: z.string().min(3, { message: "El nombre es corto" }),
   lastname: z.string().min(3, { message: "El apellido es corto" }),
+  username: z.string().min(3, { message: "El nombre de usuario incorrecto" }),
   password: z.string().min(8, { message: "La contraseña es débil" }),
   email: z.string().email({ message: "El correo no es válido" }),
   role: z.enum(["ADMIN", "TECHNICAL", "GENERIC"], {

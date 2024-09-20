ef77f77224f4012567d627075e2f6e4a4bfc4002
Esteban <estebancg12@hotmail.com>
2024-09-20 14:39:15 -0500

    Refactor login form to use API for authentication and store session token 🧰

--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import React, { useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { InputLogin } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
 import { LockKeyhole, User } from "lucide-react";
-import { toast } from "@/hooks/use-toast";
-import { useRouter } from "next/router";
+import { useRouter } from "next/dist/client/router";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const { toast } = useToast();
   const router = useRouter();
+  const ENDPOINT_LOGIN = process.env.NEXT_PUBLIC_API_URL_LOGIN;
+  console.log(ENDPOINT_LOGIN);
 
-  async function onSubmit() {
+  async function handleSubmit() {
     try {
-      const response = await fetch("BACK_URL/auth/login", {
+      const response = await fetch(`${ENDPOINT_LOGIN}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
@@ -45,48 +47,50 @@ export default function LoginForm() {
   }
 
   return (
-    <form onSubmit={onSubmit} className="mt-8 space-y-6">
-      <div className="flex justify-center items-center">
-        <InputLogin
-          id="username"
-          type="text"
-          icon={<User />}
-          placeholder="Nombre de usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div className="flex justify-center items-center">
-        <InputLogin
-          id="password"
-          type="password"
-          icon={<LockKeyhole />}
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className="text-sm flex justify-center items-center">
-        <a href="#" className="w-full flex justify-end hover:underline">
-          ¿Olvidaste la contraseña?
-        </a>
-      </div>
-      <div className="flex justify-center items-center">
-        <Button
-          type="submit"
-          className="sm:w-80 sm:h-12 text-base font-bold bg-primary hover:bg-chart-6 flex justify-center items-center"
-        >
-          Iniciar sesión
-        </Button>
-      </div>
-      <div className="text-sm text-center flex justify-center items-center">
-        ¿Cuenta no registrada?{" "}
-        <a href="#" className="text-secondary hover:underline">
-          Crea una cuenta
-        </a>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+        <div className="flex justify-center items-center">
+          <InputLogin
+            id="username"
+            type="text"
+            icon={<User />}
+            placeholder="Nombre de usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <InputLogin
+            id="password"
+            type="password"
+            icon={<LockKeyhole />}
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="text-sm flex justify-center items-center">
+          <a href="#" className="w-full flex justify-end hover:underline">
+            ¿Olvidaste la contraseña?
+          </a>
+        </div>
+        <div className="flex justify-center items-center">
+          <Button
+            type="submit"
+            className="sm:w-80 sm:h-12 text-base font-bold bg-primary hover:bg-chart-6 flex justify-center items-center"
+          >
+            Iniciar sesión
+          </Button>
+        </div>
+        <div className="text-sm text-center flex justify-center items-center">
+          ¿Cuenta no registrada?{" "}
+          <a href="#" className="text-secondary hover:underline">
+            Crea una cuenta
+          </a>
+        </div>
+      </form>
+    </>
   );
 }

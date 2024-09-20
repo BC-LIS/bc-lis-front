352400e8288858133db2ffc960a50cf19ace5082
Esteban <estebancg12@hotmail.com>
2024-09-20 15:08:57 -0500

    Refactor _document.tsx to set the title attribute in the Head component ⚙️

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="es-CO" suppressHydrationWarning>
-      <Head>
-        <title>Base de conocimiento LIS</title>
+      <Head title="Base de conocimiento LIS">
         <meta
           name="description"
           content="Página para centralizar la información de los procesos que se llevan a cabo en el Laboratorio Integrado de Sistemas"

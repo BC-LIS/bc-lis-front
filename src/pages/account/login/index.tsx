a5647eb6bdb3ca4f1d29ec0ae8658f6cda66572b
Esteban <estebancg12@hotmail.com>
2024-09-23 13:13:48 -0500

    Refactor import path for LoginForm component in login page 🧰

--- src/pages/account/login/index.tsx
+++ src/pages/account/login/index.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import LoginForm from "@components/forms/LoginForm";
+import LoginForm from "@/components/forms/loginForm";
 import Image from "next/image";
 
 export default function LoginPage() {

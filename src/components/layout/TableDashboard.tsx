c37aa40317cfcd104e16aee0d2c4c94f67396963
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-09-30 12:32:40 -0500

    refactor: :lipstick: Update TableDashboard component to show the dialog attach documents

--- src/components/layout/TableDashboard.tsx
+++ src/components/layout/TableDashboard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import React, { useRef } from 'react';
 import {
   File,
   ListFilter,
@@ -32,7 +33,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
+import { Input, InputFile } from "@/components/ui/input"
 import {
   Table,
   TableBody,
@@ -47,11 +48,30 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
+    DialogClose,
+    DialogTrigger
+} from "@/components/ui/dialog"
+import { FilePlus2 } from "lucide-react"
+  
 
 export default function TableDashboard() {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleButtonClick = () => {
+      if (fileInputRef.current) {
+        fileInputRef.current.click();
+      }
+    };
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <header className="flex h-14 items-center gap-4 justify-between px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+      <header className="flex h-14 items-center gap-4 justify-between px-4 pb-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
         <BreadcrumbList> 
             <BreadcrumbItem>
                 <BreadcrumbLink href="#">
@@ -77,15 +97,15 @@ export default function TableDashboard() {
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
-              placeholder="Search..."
+              placeholder="Buscar..."
               className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
             />
         </div>
       </header>
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <Tabs defaultValue="all">
-            <div className="flex items-center">
-                <TabsList>
+            <div className="flex items-center justify-center">
+                <TabsList >
                     <TabsTrigger value="all">Todos</TabsTrigger>
                     <TabsTrigger value="active">Activos</TabsTrigger>
                     <TabsTrigger value="draft">Borradores</TabsTrigger>
@@ -94,7 +114,7 @@ export default function TableDashboard() {
                     </TabsTrigger>
                 </TabsList>
             
-                <div className="ml-auto flex items-center gap-2">
+                <div className="ml-auto flex items-center justify-center gap-2">
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                             <Button variant="outline" size="sm" className="h-7 gap-1">
@@ -119,12 +139,72 @@ export default function TableDashboard() {
                             Exportar
                         </span>
                     </Button>
-                    <Button size="sm" className="h-7 gap-1">
-                        <PlusCircle className="h-3.5 w-3.5" />
-                        <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                            Añadir archivo
-                        </span>
-                    </Button>
+                    <Dialog>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button size="sm" variant="secondary" className="h-7 gap-1">
+                                    <PlusCircle className="h-3.5 w-3.5" />
+                                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                        Añadir archivo
+                                    </span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Añadir por</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem> Crear </DropdownMenuItem>
+                                <DialogTrigger asChild>
+                                    <DropdownMenuItem> Adjuntar </DropdownMenuItem>
+                                </DialogTrigger>
+                        </DropdownMenuContent>
+                        </DropdownMenu>
+                        <DialogContent className="sm:max-w-md">
+                            <DialogHeader>
+                            <DialogTitle>Share link</DialogTitle>
+                            <DialogDescription>
+                                Anyone who has this link will be able to view this.
+                            </DialogDescription>
+                            </DialogHeader>
+                            <div className="flex items-center space-x-2">
+                            <div className="grid flex-1 gap-2 cursor-pointer">
+                                <InputFile
+                                    type="file"
+                                    ref={fileInputRef}
+                                />
+                            </div>
+                            <div>
+                                <Button 
+                                    type="submit" 
+                                    size="sm" 
+                                    variant="secondary"
+                                    className="px-3"
+                                    onClick={handleButtonClick}
+                                >
+                                    <span className="sr-only">Copy</span>
+                                    <FilePlus2 className="h-4 w-4" />
+                                </Button>
+                                <InputFile 
+                                    type="file" 
+                                    ref={fileInputRef}
+                                    style={{display: "none"}}
+                                />
+
+                            </div>
+                            </div>
+                            <DialogFooter className="sm:justify-between">
+                                <DialogClose asChild>
+                                    <Button type="button" variant="secondary">
+                                        Cerrar
+                                    </Button>
+                                </DialogClose>
+                                <DialogClose asChild>
+                                    <Button type="submit" variant="secondary">
+                                        Adjuntar
+                                    </Button>
+                                </DialogClose>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
                 </div>
             </div>
             <TabsContent value="all">

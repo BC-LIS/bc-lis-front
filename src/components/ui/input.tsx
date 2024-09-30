59c56dc294ed63eb936be70ced12ce9a9a5f8503
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-09-30 12:25:55 -0500

    feat: :sparkles: Add InputFile component for file input functionality

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -46,7 +46,31 @@ const InputLogin = React.forwardRef<HTMLInputElement, InputProps>(
   }
 );
 
+const InputFile = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, icon, ...props }, ref) => {
+    return (
+      <div className="relative">
+        {icon && (
+          <div className="absolute inset-y-0 left-2 flex items-center">
+            {icon}
+          </div>
+        )}
+        <input
+          type={type}
+          className={cn(
+            "flex sm:h-12 sm:w-full rounded-md border border-input p-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
 Input.displayName = "Input";
 InputLogin.displayName = "InputLogin";
+InputFile.displayName = "InputFile";
 
-export { Input, InputLogin };
+export { Input, InputLogin, InputFile };

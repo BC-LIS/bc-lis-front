f3ca1fbfcad059fceac5d555401f86c352854418
Esteban <estebancg12@hotmail.com>
2024-10-05 20:05:07 -0500

    Refactor and Format dashboard's code 🧰

--- src/pages/document/register/index.tsx
+++ src/pages/document/register/index.tsx
@@ -1,45 +1,44 @@
-"use client"
+"use client";
 
-import { Dashboard } from '@/components/layout/dashboard'; 
-import { LoadingSpinner } from '@/components/ui/loading-spinner';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react'
+import { Dashboard } from "@/components/layout/dashboard";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function DocumentPage() {
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const userInfo = localStorage.getItem("userInfo");
-    
-        if (userInfo) {
-          const user = JSON.parse(userInfo);
-          if (user.role !== "ADMIN") {
-            // Redirigir a la página principal si no es administrador
-            router.push("/");
-          } else {
-            // Si es administrador, dejar de mostrar el spinner
-            setLoading(false);
-          }
-        } else {
-          // Redirigir a la página de inicio de sesión si no está autenticado
-          router.push("/account/login");
-        }
-    }, [router]);
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
 
-    // Mostrar el spinner mientras se verifica la autenticación
-    if (loading) {
-        return (
-        <div className="flex justify-center items-center h-screen">
-            <LoadingSpinner size={48} />
-        </div>
-        );
+    if (userInfo) {
+      const user = JSON.parse(userInfo);
+      if (user.role !== "ADMIN") {
+        // Redirigir a la página principal si no es administrador
+        router.push("/");
+      } else {
+        // Si es administrador, dejar de mostrar el spinner
+        setLoading(false);
+      }
+    } else {
+      // Redirigir a la página de inicio de sesión si no está autenticado
+      router.push("/account/login");
     }
+  }, [router]);
 
+  // Mostrar el spinner mientras se verifica la autenticación
+  if (loading) {
     return (
-        <section className="flex items-center justify-center my-4 p-2 gap-4 sm:p-4">
-          <Dashboard />
-        </section>
-      );
-}
+      <div className="flex justify-center items-center h-screen">
+        <LoadingSpinner size={48} />
+      </div>
+    );
+  }
 
+  return (
+    <section className="flex items-center justify-center my-4 p-2 gap-4 sm:p-4">
+      <Dashboard />
+    </section>
+  );
+}

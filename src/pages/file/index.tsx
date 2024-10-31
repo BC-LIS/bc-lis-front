6807408d248ed457911eb0b824693d8a912b5317
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-31 00:17:07 -0500

    refactor: :recycle: Update FilePage component to handle user authentication and display loading spinner

--- src/pages/file/index.tsx
+++ src/pages/file/index.tsx
@@ -1,7 +1,41 @@
+"use client";
+
 import { Dashboard } from "@/components/layout/dashboard/Dashboard";
-import React from "react";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function FilePage() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+
+    if (userInfo) {
+      const user = JSON.parse(userInfo);
+      if (user.role !== "ADMIN" && user.role !== "TECHNICAL") {
+        // Redirigir a la página principal si no es administrador
+        router.push("/");
+      } else {
+        // Si es administrador, dejar de mostrar el spinner
+        setLoading(false);
+      }
+    } else {
+      // Redirigir a la página de inicio de sesión si no está autenticado
+      router.push("/account/login");
+    }
+  }, [router]);
+
+  // Mostrar el spinner mientras se verifica la autenticación
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <LoadingSpinner size={48} />
+      </div>
+    );
+  }
+
   return (
     <section className="flex items-center justify-center my-4 p-2 gap-4 sm:p-4 h-full w-full">
       <Dashboard />
